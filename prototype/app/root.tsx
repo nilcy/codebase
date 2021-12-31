195ab6e8af3febe853d8635362fb9655feb7e82f
Takehiro Shimokawa <takehiro.shimokawa@gmail.com>
2021-12-31 22:24:12 +0900

    prototype を更新

--- prototype/app/root.tsx
+++ prototype/app/root.tsx
@@ -9,12 +9,12 @@ import {
 import type { MetaFunction } from "remix";
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: "Prototype App" };
 };
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
